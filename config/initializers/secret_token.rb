282f6a74f6eee3b2a55fea59d17d6975615dbee7
Frederic Lemay <flemay@thoughtworks.com>
2012-06-22 11:14:16 +1000

    set the token to 30 charactersas required by rails

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,5 +4,5 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Agra::Application.config.secret_token = 'changeme'
-Agra::Application.config.sha1_salt = 'changeme'
+Agra::Application.config.secret_token = 'changeme  12345678901234567890'
+Agra::Application.config.sha1_salt = 'changeme  12345678901234567890'
